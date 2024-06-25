7139afce4b34920e5588eb207d781961987932c7
Capital_Woodlands <khouri301@gmail.com>
2024-06-25 01:13:45 -0400

    Changes to be committed: 	modified:   IDK/obj/Debug/net8.0/IDK.AssemblyInfo.cs 	modified:   IDK/obj/Debug/net8.0/IDK.AssemblyInfoInputs.cache 	modified:   Idkwdywkd/obj/Debug/net8.0/Idkwdywkd.AssemblyInfo.cs 	modified:   Idkwdywkd/obj/Debug/net8.0/Idkwdywkd.AssemblyInfoInputs.cache

--- Idkwdywkd/obj/Debug/net8.0/Idkwdywkd.AssemblyInfo.cs
+++ Idkwdywkd/obj/Debug/net8.0/Idkwdywkd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Idkwdywkd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79247d0ef8ab443a02081b0ebf9557bc8f4a042a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b70c802e972923ee8fc7f0040b48aa5db3821fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Idkwdywkd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Idkwdywkd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
