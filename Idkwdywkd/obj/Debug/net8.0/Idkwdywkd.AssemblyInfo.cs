852b0745ec16d603b5c9f9927388142366d7ed84
Capital_Woodlands <khouri301@gmail.com>
2024-07-19 18:31:43 -0400

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   IDK/obj/Debug/net8.0/IDK.AssemblyInfo.cs
    	modified:   IDK/obj/Debug/net8.0/IDK.AssemblyInfoInputs.cache
    	modified:   Idkwdywkd/obj/Debug/net8.0/Idkwdywkd.AssemblyInfo.cs
    	modified:   Idkwdywkd/obj/Debug/net8.0/Idkwdywkd.AssemblyInfoInputs.cache
    	modified:   Midcoin/obj/Debug/net8.0/Midcoin.AssemblyInfo.cs
    	modified:   Midcoin/obj/Debug/net8.0/Midcoin.AssemblyInfoInputs.cache
    	modified:   idm/obj/Debug/net8.0/idm.AssemblyInfo.cs
    	modified:   idm/obj/Debug/net8.0/idm.AssemblyInfoInputs.cache
    	modified:   soup/Program.cs
    	modified:   soup/bin/Debug/net8.0/soup.dll
    	modified:   soup/bin/Debug/net8.0/soup.exe
    	modified:   soup/bin/Debug/net8.0/soup.pdb
    	modified:   soup/obj/Debug/net8.0/apphost.exe
    	modified:   soup/obj/Debug/net8.0/ref/soup.dll
    	modified:   soup/obj/Debug/net8.0/refint/soup.dll
    	modified:   soup/obj/Debug/net8.0/soup.AssemblyInfo.cs
    	modified:   soup/obj/Debug/net8.0/soup.AssemblyInfoInputs.cache
    	modified:   soup/obj/Debug/net8.0/soup.dll
    	modified:   soup/obj/Debug/net8.0/soup.pdb
    	modified:   soup/obj/Debug/net8.0/soup.sourcelink.json
    	modified:   sum noo/obj/Debug/net8.0/sum noo.AssemblyInfo.cs
    	modified:   sum noo/obj/Debug/net8.0/sum noo.AssemblyInfoInputs.cache

--- Idkwdywkd/obj/Debug/net8.0/Idkwdywkd.AssemblyInfo.cs
+++ Idkwdywkd/obj/Debug/net8.0/Idkwdywkd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Idkwdywkd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b079dec4fa8b417bdacae948b7388292d6f8688")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d2b812fdb82bec6f68bb7d8019f194165dbc1b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Idkwdywkd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Idkwdywkd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- soup/Program.cs
+++ soup/Program.cs
@@ -1,12 +1,90 @@
-﻿Console.Clear();
+﻿ Console.Clear();
+Console.WriteLine("Calculate? (Y/N)");
+bool iscalcing = true;
+
+while (iscalcing == true);
+{
+ Console.WriteLine(" -- SUperC Cool iNTEGeer calcuaor -- ");
+        Console.WriteLine(" ");
+        Console.WriteLine(" eNTER  INT 1");
+        float num1;
+        while (!float.TryParse(Console.ReadLine(), out num1))
+        {
+            Console.WriteLine("Invalid input. Please enter a valid integer.");
+        }
+
+        //---------------------------------------------------------------//
+        Console.WriteLine(" eNTER  INT 2");
+        float num2;
+        while (!float.TryParse(Console.ReadLine(), out num2))
+        {
+            Console.WriteLine("Invalid input. Please enter a valid integer.");
+        }
+
+        Console.WriteLine("WhaT OperAtIoN TO PerFoRM? (+,-,x,/)");
+        Console.WriteLine("---------------------------------------------------------------");
+
+        string nuse = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(nuse) && nuse.Length == 1)
+        {
+            char mathop = nuse[0];
+            float result = 0;
+            bool validOperation = true;
+
+            switch (mathop)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case 'x':
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Division by zero is not allowed.");
+                        validOperation = false;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("----- That is not an applicable operation -----");
+                    validOperation = false;
+                    break;
+            }
+
+            if (validOperation)
+            {
+                Console.WriteLine($"Result: {result}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("----- That is not an applicable operation -----");
+        }
+
+    Console.WriteLine("Calculate again?");   
+    string iscl =Console.ReadLine();
+if (iscl = "y")
+{
+    iscalcing == true;
+}
+else iscalcing is false
+
+if (iscalcing == false)
+{
+    Console.ReadKey();
+}
+
+}
 
 
 
-Console.WriteLine(" EGGMASTER! ");
-
-Console.ReadKey();
-
-
-
-
-// ARVAO
